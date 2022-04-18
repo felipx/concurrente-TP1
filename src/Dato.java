0169da52460d2a145ba8e6669747c4ae572cefe1
14matias13 <matiasbengoechea@unc.edu.ar>
2022-04-18 12:04:39 -0300

    Update Dato.java
    Añadida la clase Dato

--- src/Dato.java
+++ src/Dato.java
@@ -1,11 +1,60 @@
 import java.util.HashSet;
 
 public class Dato {
-    private int id;
-    private HashSet<Revisor> reviews;
 
-    public int getId() {
-        return this.id;
+    private HashSet reviews;
+
+    /**
+     * Constructor sin parámetros
+     *
+     */
+    public Dato(){
+            reviews = new HashSet();
     }
 
+    /**
+     * Agrega un revisor al HashSet usando el ID del thread.
+     * Verifica que no lo haya revisado antes, si no imprime un error
+     *
+     * @param thread El revisor a agregar
+     */
+    public void addReviewer(Thread thread){
+        if(checkReviewer(thread)){
+                reviews.add(thread.getId());
+            }
+            else{
+                System.out.println(String.format("El hilo %d ya ha validado este dato", thread.getId()));
+            }
+        }
+
+    /**
+     * Devuelve la cantidad de revisores que han validado el dato
+     *
+     */
+    public int getReviewersCount(){
+            return reviews.size();
+        }
+
+    /**
+     * Verifica que el validador no haya revisado el dato anteriormente
+     *
+     * @param thread El revisor del cual se quiere validar
+     */
+    public boolean checkReviewer(Thread thread){
+            return reviews.contains(thread.getId());
+        }
+  
+
+    /**
+     * Realiza la copia de un dato
+     *
+     */
+    public Object clone(){
+            Dato dato = null;
+            try {
+                dato = (Dato) super.clone();
+            } catch (CloneNotSupportedException e) {}
+
+            return dato;
+        }
     }
